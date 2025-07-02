854725258d44cbb02bf27fee00df87943f611132
JacobKnoth <jakek1406@gmail.com>
2025-07-02 23:38:49 +0000

    updated build and launch scripts

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ source /opt/ros/$ROS_DISTRO/setup.bash
 
 colcon build \
     --symlink-install \
-    --base-paths /home/trickfire/urc-2023\
+    --base-paths /home/trickfire/jake-urc-2023/urc-2023\
     --cmake-args \
         -DCMAKE_BUILD_TYPE=Debug
