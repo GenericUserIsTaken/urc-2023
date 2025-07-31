ef72a3489c30c26a5c63c7bd847560d081272a8f
JacobKnoth <jakek1406@gmail.com>
2025-07-31 01:10:01 +0000

    made a small change to the build.sh to maybe fix some issues

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ colcon build \
     --symlink-install \
     --base-paths /home/trickfire/jake-urc-2023/urc-2023\
     --cmake-args \
-        -DCMAKE_BUILD_TYPE=Debug
+        -DCMAKE_BUILD_TYPE=Release
