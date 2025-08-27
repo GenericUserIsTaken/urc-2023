d3fb8d82ca0aceac98555edada66ca3a16049838
JacobKnoth <jakek1406@gmail.com>
2025-08-27 23:57:10 +0000

    trying to fix weird build.sh problems

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ source /opt/ros/$ROS_DISTRO/setup.bash
 colcon build \
     --symlink-install \
     --base-paths /home/trickfire/jake-urc-2023/urc-2023\
+    --allow-overriding cv_bridge image_geometry \
     --cmake-args \
         -DCMAKE_BUILD_TYPE=Release
