3e9bd69f5c1825eee713a3d142f47da59acafa0f
nukkeldev <nukkel.dev@gmail.com>
2025-10-09 19:15:36 -0700

    Update Dockerfile for ZED SDK, fix zed-ros2-wrapper clone

--- build.sh
+++ build.sh
@@ -1,25 +1,30 @@
 #!/usr/bin/env bash
 
-# Run ZED dependency setup first
+# Run ZED dependency setup.
 ./setup_zed_dependencies.sh
 
+# Make sure ROS packages are accessable.
 source /opt/ros/$ROS_DISTRO/setup.bash
 
-# build zed components first
+# Make sure we have rosdep dependencies (please update apt and rosdep beforehand).
+rosdep install --from-paths src --ignore-src -r -y
+
+# Build the ZED components.
 colcon build \
---symlink-install \
---base-paths /home/trickfire/urc-2023 \
---packages-select zed_components \
---cmake-args \
--DCMAKE_BUILD_TYPE=RelWithDebInfo
+    --symlink-install \
+    --base-paths /home/trickfire/urc-2023 \
+    --packages-select zed_components \
+    --cmake-args \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo
 
 colcon build \
---symlink-install \
---base-paths /home/trickfire/urc-2023 \
---packages-select zed_wrapper \
---cmake-args \
--DCMAKE_BUILD_TYPE=RelWithDebInfo
+    --symlink-install \
+    --base-paths /home/trickfire/urc-2023 \
+    --packages-select zed_wrapper \
+    --cmake-args \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo
 
+# Build everything else.
 colcon build \
     --symlink-install \
     --base-paths /home/trickfire/urc-2023\
